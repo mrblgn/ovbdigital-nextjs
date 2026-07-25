446b7262d3d9665749e19d58b438af14444949a2
mrblgn <omerbilgin1@gmail.com>
2026-07-25 20:47:21 +0200

    chore: hero bg image styling fix

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <section className="font-montserrat">
       <h1 className="hidden">OvB Digital</h1>
-      <div className="flex items-center justify-center w-full h-screen text-white bg-[url(@/assets/images/o-hero-4.jpg)]">
+      <div className="flex items-center justify-center w-full h-screen text-white bg-[url(@/assets/images/o-hero-4.jpg)] bg-no-repeat bg-cover bg-center">
         <div className="absolute left-0 top-0 right-0 bottom-0 z-0 bg-black/35" />
         <div className="flex flex-col max-w-3/5 items-center justify-center">
           <h2 className="text-6xl leading-16 text-center mb-8 font-semibold whitespace-pre-line z-10">
